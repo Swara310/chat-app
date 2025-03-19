f120d500b2529266e430de1386a73fb9664d482c
unknown <9146939196>
2025-03-19 18:16:08 +0530

    updated profilePage

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -6,19 +6,20 @@ const ProfilePage = () => {
      const {authUser,isUpdatingProfile,updateProfile} = useAuthStore();
      const [selectedImg, setSelectedImg] = useState(null);
 
-     const handleImageUpload= async(e) =>{
+     const handleImageUpload = async (e) => {
       const file = e.target.files[0];
-      if(!file) return ;
-
+      if (!file) return;
+  
       const reader = new FileReader();
+  
       reader.readAsDataURL(file);
-
-      reader.onload= async ()=> {
+  
+      reader.onload = async () => {
         const base64Image = reader.result;
         setSelectedImg(base64Image);
-        await updateProfile({profilePic:base64Image});
-      }
-     };
+        await updateProfile({ profilePic: base64Image });
+      };
+    };
 
   return (
     <div className="h-screen pt-20">
